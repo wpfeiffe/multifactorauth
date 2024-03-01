51bcd6eebc81ddece135a604d8ccde5715fea3a4
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2024-02-29 22:10:57 -0500

    Add htmx stuff

--- mfa-server/src/main/java/com/wspfeiffer/mfaserver/controllers/HtmxController.java
+++ mfa-server/src/main/java/com/wspfeiffer/mfaserver/controllers/HtmxController.java
@@ -1,9 +1,12 @@
 package com.wspfeiffer.mfaserver.controllers;
 
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("htmx")
@@ -13,5 +16,16 @@ public class HtmxController {
     String getFirst(Model model) {
         model.addAttribute("appname", "MfaServer-HTMX");
         return "htmx-first";
+    }
+
+    @HxRequest("trigger-btn")
+    @GetMapping("/populate-names")
+    String populateNames (Model model) {
+        model.addAttribute("names", List.of(
+                "Bill Pfeiffer",
+                "Deanna Pfeiffer",
+                "Brian Pfeiffer",
+                "Liam Pfeiffer"));
+        return "populate-names";
     }
 }
